998d08a78b1bc16dc3f5e545e2427d1a1fdac998
shamsheer ahmad <shamsheerahmed571@gmail.com>
2024-02-29 13:10:02 +0530

    deploy

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import app from './server'
+import config from './config'
+
+
+app.listen(config.port, () => {
+    console.log(`hello world on http://localhost:-${config.port}`)
+})
+
